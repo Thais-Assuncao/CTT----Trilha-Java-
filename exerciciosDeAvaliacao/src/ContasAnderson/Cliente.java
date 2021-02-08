e5e578f1a34cb026c20e3890b2eead2037581a1b
ThaisAssuncao <thais.ap.assuncao12@gmail.com>
2021-02-08 18:45:29 -0300

    Implementação da ContaBancaria e Semana de Banco de Dados

---
+++ exerciciosDeAvaliacao/src/ContasAnderson/Cliente.java
@@ -0,0 +1,14 @@
+package ContasAnderson;
+
+public class Cliente {
+	
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+}
---
+++ exerciciosDeAvaliacao/src/ContasAnderson/ContaPoupanca.java
@@ -0,0 +1,10 @@
+package ContasAnderson;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Double saldo, Cliente cliente) {
+		super(saldo, cliente);
+		// TODO Auto-generated constructor stub
+	}
+
+}
